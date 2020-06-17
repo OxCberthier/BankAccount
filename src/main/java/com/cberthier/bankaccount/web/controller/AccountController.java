1a97daf7cd6fab02b83178bc33c6d50170cdb381
BERTHIER Cyril <berthier.cyril.fr@gmail.com>
2020-06-17 23:53:19 +0200

    [US 1] Add REST endpoint to add deposit operation to account: Impl

--- src/main/java/com/cberthier/bankaccount/web/controller/AccountController.java
+++ src/main/java/com/cberthier/bankaccount/web/controller/AccountController.java
@@ -1,9 +1,31 @@
 package com.cberthier.bankaccount.web.controller;
 
+import com.cberthier.bankaccount.domain.model.AccountNotFoundException;
+import com.cberthier.bankaccount.domain.model.InvalidOperationException;
+import com.cberthier.bankaccount.service.AccountService;
+import com.cberthier.bankaccount.web.payload.OperationPayload;
+import com.cberthier.bankaccount.web.result.ApiResult;
+import com.cberthier.bankaccount.web.result.Result;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/accounts")
 public class AccountController {
+
+    @Autowired
+    AccountService accountService;
+
+    @PostMapping("/operations")
+    public ResponseEntity<ApiResult> addOperation(@Validated @RequestBody OperationPayload operationPayload) throws InvalidOperationException, AccountNotFoundException {
+
+        accountService.addOperation(operationPayload.toCommand());
+
+        return Result.ok();
+    }
 }
