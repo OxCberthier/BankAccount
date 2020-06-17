89b92a07596ab7bff0a5f1ae1e19a1b9d7e85996
BERTHIER Cyril <berthier.cyril.fr@gmail.com>
2020-06-17 14:37:35 +0200

    Add domain model

---
+++ src/main/java/com/cberthier/bankaccount/domain/model/Operation.java
@@ -0,0 +1,56 @@
+package com.cberthier.bankaccount.domain.model;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class Operation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(foreignKey = @ForeignKey(name = "fk_operation_account_id"), nullable = false)
+    private Account account;
+
+    @Column(nullable = false)
+    private double amount;
+
+    @Column(nullable = false)
+    private OperationTypeEnum operationType;
+
+    @Column(nullable = false, columnDefinition = "TIMESTAMP")
+    private LocalDateTime date;
+
+    //Default Hibernate constructor
+    public Operation() {
+    }
+
+    public Operation(Account account, double amount, OperationTypeEnum operationType, LocalDateTime date) {
+        this.account = account;
+        this.amount = amount;
+        this.operationType = operationType;
+        this.date = date;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public OperationTypeEnum getOperationType() {
+        return operationType;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+}
