ee06983f7968c2879a9d05ae6a3ef0e318aa3616
BERTHIER Cyril <berthier.cyril.fr@gmail.com>
2020-06-17 15:33:45 +0200

    [US 1] Add Deposit Operation to account and update account balance: Implement method

--- src/main/java/com/cberthier/bankaccount/service/impl/AccountServiceImpl.java
+++ src/main/java/com/cberthier/bankaccount/service/impl/AccountServiceImpl.java
@@ -2,12 +2,16 @@ package com.cberthier.bankaccount.service.impl;
 
 import com.cberthier.bankaccount.domain.OperationCommand;
 import com.cberthier.bankaccount.domain.model.Account;
+import com.cberthier.bankaccount.domain.model.Operation;
 import com.cberthier.bankaccount.domain.repository.AccountCrudRepository;
 import com.cberthier.bankaccount.domain.repository.OperationPagingAndSortingRepository;
 import com.cberthier.bankaccount.service.AccountService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
 public class AccountServiceImpl implements AccountService {
@@ -26,6 +30,20 @@ public class AccountServiceImpl implements AccountService {
 
     @Override
     public Account addOperation(OperationCommand operationCommand) {
-        return null;
+        Optional<Account> optionalAccount = accountCrudRepository.findById(operationCommand.getAccountId());
+        Account account = optionalAccount.get();
+
+        logger.debug("Add Operation -> Account {}, Amount: {}, Type: {}",
+                account.getName(), operationCommand.getAmount(), operationCommand.getOperationType());
+        logger.debug("Old balance -> {}", account.getBalance());
+
+        account.setBalance(Double.sum(account.getBalance(), operationCommand.getAmount()));
+
+        logger.debug("New balance -> {}", account.getBalance());
+
+        Operation operation = new Operation(account, operationCommand.getAmount(), operationCommand.getOperationType(),
+                LocalDateTime.now());
+        operationPagingAndSortingRepository.save(operation);
+        return operation.getAccount();
     }
 }
