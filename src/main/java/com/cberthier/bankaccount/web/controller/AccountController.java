044b4ea8943b859b8233895383cfecfc57bc3e61
BERTHIER Cyril <berthier.cyril.fr@gmail.com>
2020-06-20 22:56:58 +0200

    [US 1] Add REST endpoint to retrieve operation account: Impl

--- src/main/java/com/cberthier/bankaccount/web/controller/AccountController.java
+++ src/main/java/com/cberthier/bankaccount/web/controller/AccountController.java
@@ -1,22 +1,19 @@
 package com.cberthier.bankaccount.web.controller;
 
-import com.cberthier.bankaccount.domain.model.Account;
-import com.cberthier.bankaccount.domain.model.AccountNotFoundException;
-import com.cberthier.bankaccount.domain.model.InsufficientFundsException;
-import com.cberthier.bankaccount.domain.model.InvalidOperationException;
+import com.cberthier.bankaccount.domain.model.*;
 import com.cberthier.bankaccount.service.AccountService;
 import com.cberthier.bankaccount.web.payload.OperationPayload;
 import com.cberthier.bankaccount.web.result.AccountResponseEntity;
 import com.cberthier.bankaccount.web.result.ApiResult;
+import com.cberthier.bankaccount.web.result.OperationsResponseEntity;
 import com.cberthier.bankaccount.web.result.Result;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/accounts")
@@ -38,6 +35,13 @@ public class AccountController {
         }
 
         return AccountResponseEntity.build(account);
+    }
+
+    @GetMapping("/operations")
+    public ResponseEntity<ApiResult> getOperations(@RequestParam Long accountId, Pageable pageable) {
+        Page<Operation> operations = accountService.getOperations(accountId, pageable);
+
+        return OperationsResponseEntity.build(operations);
     }
 
     @ExceptionHandler(AccountNotFoundException.class)
---
+++ src/main/java/com/cberthier/bankaccount/web/result/OperationsResponseEntity.java
@@ -0,0 +1,26 @@
+package com.cberthier.bankaccount.web.result;
+
+import com.cberthier.bankaccount.domain.model.Operation;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+
+import java.util.stream.Collectors;
+
+public class OperationsResponseEntity {
+
+    public static ResponseEntity<ApiResult> build(Page<Operation> operations) {
+        ApiResult apiResult = ApiResult.blank()
+                .add("page", operations.getNumber())
+                .add("totalPages", operations.getTotalPages())
+                .add("totalElements", operations.getTotalElements())
+                .add("size", operations.getSize())
+                .add("operations", operations.stream()
+                        .map(operation -> ApiResult.blank()
+                                .add("date", operation.getDate())
+                                .add("amount", operation.getAmount())
+                                .add("type", operation.getOperationType())).collect(Collectors.toList())
+                );
+
+        return Result.ok(apiResult);
+    }
+}
